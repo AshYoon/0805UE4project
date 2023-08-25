fee53e92c8f41a0f03fa61362410a4f2dd5dd041
AshYoon <rlawodbs7691@naver.com>
2023-08-25 22:25:25 +0900

    PickUp_1

---
+++ Source/U05_Action/Inventory/Pickup.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Inventory/Pickup.h"
+//#include "Inventory.h"
+
+
+APickup::APickup()
+{
+
+}
+
+void APickup::Interact_Implementation()
+{
+
+
+	// all interactive item will use this function 
+
+	//AInventoryCharacter
+
+
+
+}
---
+++ Source/U05_Action/Inventory/Pickup.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Inventory/Interactable.h"
+#include "Pickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class U05_ACTION_API APickup : public AInteractable
+{
+	GENERATED_BODY()
+
+public:
+		APickup();
+
+		virtual void Interact_Implementation() override; 
+
+
+
+		//UFUNCTION(BlueprintNativeEvent)
+			//void Use();
+
+		
+
+
+
+
+		UPROPERTY(EditAnywhere, Category = "Pickup Properties")
+			UTexture2D* PickupTexture; // pickup 아이콘 or 썸네일 
+
+
+		UPROPERTY(EditAnywhere, Category = "Pickup Properties")
+			FString ItemName;
+
+
+		
+
+
+		UPROPERTY(EditAnywhere, Category = "Pickup Properties")
+			int32 Value;
+
+
+
+		// this is clled when the item is picked up and needs to be cleaned up from the level 
+		void OnPickedUp();
+
+	
+};
