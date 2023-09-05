25e45b965172d2646ac3859311babcd5d4bfe2bd
AshYoon <rlawodbs7691@naver.com>
2023-09-05 19:55:05 +0900

    PickupCoins01

---
+++ Source/U05_Action/Inventory/Pickup_Coins.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Inventory/Pickup_Coins.h"
+#include "Characters/CPlayer.h"
+
+
+APickup_Coins::APickup_Coins()
+{
+
+}
+
+void APickup_Coins::Interact_Implementation()
+{
+
+
+}
+
+
---
+++ Source/U05_Action/Inventory/Pickup_Coins.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Inventory/Pickup.h"
+#include "Pickup_Coins.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class U05_ACTION_API APickup_Coins : public APickup
+{
+	GENERATED_BODY()
+	
+
+public:
+	APickup_Coins();
+
+	virtual void Interact_Implementation() override;
+
+
+};
